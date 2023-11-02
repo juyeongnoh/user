1c2fb2b05f09c70ff11ecaad3e4e67c5924c1d8c
juyeongnoh <juyeongnoh@gmail.com>
2023-11-02 22:56:08 +0900

    clean up app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,7 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<HomePage />}></Route>
-      </Routes>
-    </BrowserRouter>
-  );
+  return <div>UserPage</div>;
 }
 
 export default App;
